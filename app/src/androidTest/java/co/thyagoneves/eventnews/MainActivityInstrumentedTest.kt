9f479c1d0bbf7aabbf09a1117d81ab1e69ef918a
Thyago Neves <thyagoneves.sa@icloud.com>
2022-10-07 15:30:40 -0300

    adiciona classe helper para auxiliar testes de LiveData

--- app/src/androidTest/java/co/thyagoneves/eventnews/MainActivityInstrumentedTest.kt
+++ app/src/androidTest/java/co/thyagoneves/eventnews/MainActivityInstrumentedTest.kt
@@ -22,7 +22,7 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
-class ExampleInstrumentedTest {
+class MainActivityInstrumentedTest {
 
     @get:Rule
     var activityTestRule: ActivityScenarioRule<MainActivity> = ActivityScenarioRule(MainActivity::class.java)
