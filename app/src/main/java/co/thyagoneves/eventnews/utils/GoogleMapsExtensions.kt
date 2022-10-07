efe5cbc432245b5224b1a8f7cbfcc53bd19c3c14
Thyago Neves <thyagoneves.sa@icloud.com>
2022-10-07 16:03:37 -0300

    valida se Google maps está instalado no dispositivo

--- app/src/main/java/co/thyagoneves/eventnews/utils/GoogleMapsExtensions.kt
+++ app/src/main/java/co/thyagoneves/eventnews/utils/GoogleMapsExtensions.kt
@@ -3,6 +3,9 @@ package co.thyagoneves.eventnews.utils
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
+
 
 fun openGoogleMapsWithLatLong(latitude: Double, longitude: Double, context: Context) {
     val gmmIntentUri = Uri.parse("geo:$latitude,$longitude")
@@ -10,5 +13,9 @@ fun openGoogleMapsWithLatLong(latitude: Double, longitude: Double, context: Cont
     mapIntent.setPackage("com.google.android.apps.maps")
     if (mapIntent.resolveActivity(context.packageManager) != null) {
         context.startActivity(mapIntent)
+    } else {
+        val gmmIntentUri = Uri.parse("geo:$latitude,$longitude")
+        val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            context.startActivity(mapIntent)
     }
 }
